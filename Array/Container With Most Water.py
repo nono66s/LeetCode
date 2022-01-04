5c41676cd53ffcc18f24216e5a8d1622ecee154a
nono66s <50786471+nono66s@users.noreply.github.com>
2022-01-04 15:14:49 +0800

    Add files via upload

---
+++ Array/Container With Most Water.py
@@ -0,0 +1,50 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        Area=0
+        if len(height) == 0 :
+            return Area
+        if len(height) == 1:
+            return Area
+
+        for i in range (0,len(height)):
+            start=height[i]
+            for j in range (i+1,len(height)):
+                end=height[j]
+                if end >= start:
+                    Area=max(Area,start*(j-i))
+                else:
+                    Area=max(Area,end*(j-i))
+        return Area
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        Area=0
+        door=0
+        if len(height) == 0 :
+            return Area
+        if len(height) == 1:
+            return Area
+
+        for i in range (0,len(height)):
+            start=height[i]
+            if start > door:
+                door=start
+                for j in range (i+1,len(height)):
+                    end=height[j]
+                    if end >= door:
+                        Area=max(Area,start*(j-i))
+                    else:
+                        Area=max(Area,end*(j-i))
+        return Area
+                        
+    
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        Area,floor,l,r=0,0,0,len(height)-1
+        while r>l:
+            Area = max(Area,min(height[l],height[r])*(r-l))
+            if height[l]>height[r]:
+                r-=1
+            else:
+                l+=1
+        return Area
