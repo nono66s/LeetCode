8c1822de07d4f2e67f513948cf7206a66d7bf732
nono66s <50786471+nono66s@users.noreply.github.com>
2022-01-04 16:07:12 +0800

    Add files via upload

--- Array/Two Pointer/Container With Most Water.py
+++ Array/Two Pointer/Container With Most Water.py
@@ -40,11 +40,30 @@ class Solution:
     
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        Area,floor,l,r=0,0,0,len(height)-1
+        Area,l,r=0,0,len(height)-1
         while r>l:
             Area = max(Area,min(height[l],height[r])*(r-l))
             if height[l]>height[r]:
                 r-=1
             else:
                 l+=1
+        return Area
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        Area,floor,l,r=0,0,0,len(height)-1
+        while r>l:
+            Area = max(Area,min(height[l],height[r])*(r-l))
+            if height[l]>height[r]:
+                floor=height[r]
+                while floor >= height[r]:
+                    if r==l:
+                        break
+                    r-=1
+            else:
+                floor=height[l]
+                while floor >= height[l]:
+                    if l==r:
+                        break
+                    l+=1
         return Area
